671b322c10e1614ebc939d18133e6e4e42893a96
Jayashree Narayan <102368145+JayashreeNarayan@users.noreply.github.com>
2024-06-05 09:51:47 +1000

    Update start.py

--- Data/Data_SimEnd/Othin/start.py
+++ Data/Data_SimEnd/Othin/start.py
@@ -2,18 +2,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 import cfpack as cfp
 import argparse
+import os
+
+os.chdir("Data/Data_SimEnd/Othin")
 
 parser = argparse.ArgumentParser(description='Plot files')
 
 parser.add_argument('npy_file_names', metavar='N', type=str, nargs='+',
                     help='file name for cfpack to plot them')
 
-#parser.add_argument('--sum', dest='accumulate', action='store_const',const=sum, default=max, help='sum the integers (default: find the max)')
-
 args = parser.parse_args()
-#print(args.accumulate(args.integers))
 
-#print(args.file_names)
 data=np.load(args.npy_file_names[0])
 cfp.plot_map(data, show=True, cmap = 'seismic')
 
