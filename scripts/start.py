81ff6b2e035370c29af76f72ad659f69ccd8bc43
JayashreeNarayan <jayashreenarayangs@gmail.com>
2024-06-11 11:47:09 +1000

    Update start.py

--- scripts/start.py
+++ scripts/start.py
@@ -11,12 +11,18 @@ def zero_moment(PPV, dv=0.05):
     mom0 = np.sum(PPV, axis=2) * dv
     return mom0
 
+# function that creates the 1st-moment map from a PPV cube with the velocity axis=2, v-channel width dv and veocity array 'Vrange'
+def first_moment(PPV, Vrange, dv=0.05):
+    mom1 = np.sum(PPV*Vrange,axis=2) * dv
+    mom0 = zero_moment(PPV)
+    return mom1/mom0
+
 # ===== the following applies in case we are running this in script mode =====
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='Plot files')
 
-    choices = ['cfp', 'flashplotlib', 'PPV']
+    choices = ['cfp', 'flashplotlib', 'PPV0', 'PPV1']
     parser.add_argument('-a', '--action', metavar='action', nargs='*', default=choices, choices=choices,
                         help='choice: between flashplotlib plotting (flash) and cfpack plotting (cfp)')
 
@@ -50,11 +56,20 @@ if __name__ == "__main__":
                 cmd = "flashplotlib.py -i "+file+" -d vel"+dir+" -nolog -cmap seismic -mw -direction "+dir+" -outtype pdf -outdir "+outpath+" -vmin "+str(vmin*1e5)+" -vmax "+str(vmax*1e5)
                 cfp.run_shell_command(cmd)
 
-        # PPV cubes
+        # PPV cubes - 0 moment map
         if action == choices[2]:
             files = ["PPV_0_0.npy","PPV_90_0.npy"]
             for file in files:
                 data = np.load(path+"Data_1tff/"+file)
                 mom0 = zero_moment(data)
                 cfp.plot_map(mom0, cmap='seismic', cmap_label=r"Density (g/cm$^3$)", save=outpath+file[:-4]+"_mom0.pdf")
+        
+        # PPV cubes - first moment map
+        if action == choices[3]:
+            files = ["PPV_0_0.npy","PPV_90_0.npy"]
+            for file in files:
+                data = np.load(path+"Data_1tff/"+file)
+                Vrange = np.load(path+"Data_1tff/"+"Vrange.npy")
+                mom1 = first_moment(data, Vrange)
+                cfp.plot_map(mom1, cmap='seismic', cmap_label=r"$v$ (km/s)", save=outpath+file[:-4]+"_mom1.pdf")
 
