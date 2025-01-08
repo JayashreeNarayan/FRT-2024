469020f0edb77aaf85012d21fdf28c457cffe80a
JayashreeNarayan <jayashreenarayangs@gmail.com>
2025-01-08 23:15:38 +1100

    checks to data

--- scripts/all_FTs.py
+++ scripts/all_FTs.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# This file plots the power spectra of the moment maps at 1tff (Fig. 4) and at SE (Fig. 8) -- TOTAL 3 IMAGES
+# This file plots the power spectra of the moment maps at 1tff (Fig. 4) and at SE (Fig. 7) -- TOTAL 3 IMAGES
 
 import numpy as np
 import matplotlib as mpl
@@ -17,7 +17,7 @@ from util_scripts.universal_variables import *
 moments_45_ = moments_45()
 fmp = first_moment_plotter()
 
-def FT_1tff_before(): # plots Fig. 6 top panel
+def FT_1tff_before(): # plots Fig. 4 top panel
     path='all_FTs/' # save these figures to a different subfolder
     if not os.path.isdir(outpath+path):
         cfp.run_shell_command('mkdir '+outpath+path)
@@ -27,7 +27,7 @@ def FT_1tff_before(): # plots Fig. 6 top panel
     FT_obj.append(moments_45_.first_mom_co21_FT_obj[0])
     fmp.FT_panel(FT_obj, file=outpath+path+"before_isolation.pdf", text=img_types[0])
 
-def FT_1tff_after(): # plots Fig. 5 bottom panel
+def FT_1tff_after(): # plots Fig. 4 bottom panel
     path='all_FTs/' # save these figures to a different subfolder
     if not os.path.isdir(outpath+path):
         cfp.run_shell_command('mkdir '+outpath+path)
